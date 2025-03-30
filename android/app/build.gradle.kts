488c73f9ac44c7fa9ee7c8e4022ef387815ba950
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-03-30 23:54:26 +0700

    added app structure and firebase connection

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.frontend"
+    namespace = "com.example.taskora"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -37,6 +38,20 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 flutter {
---
+++ repostiories/schedule_repository/lib/schedule_repository.dart
@@ -0,0 +1,5 @@
+library;
+
+export 'src/entity/entity.dart';
+export 'src/model/model.dart';
+export 'src/schedule_repo.dart';
